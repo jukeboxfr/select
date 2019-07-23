78ea229a5aa4e17c7292c127a9f5fa55b7453f2d
Keny SAINT HILAIRE <kesaint-@e1r1p14.42.fr>
2019-07-23 15:13:22 +0200

    :hammer:

--- srcs/colors.c
+++ srcs/colors.c
@@ -14,46 +14,35 @@
 
 static void		print_color(int options)
 {
-	if ((options & OPT_RE_SCREEN) == OPT_RE_SCREEN)
-		return (ft_putstr("4m"));
+	int		selected;
+
+	selected = (options & OPT_RE_SCREEN) == OPT_RE_SCREEN;
 	if ((options & OPT_RED) == OPT_RED)
-		return (ft_putstr("31m"));
-	if ((options & OPT_GREEN) == OPT_GREEN)
-		return (ft_putstr("32m"));
+		return (ft_putstr(selected ? "30;41m" : "31m"));
+	if ((options & OPT_GREEN) == OPT_GREEN)	
+		return (ft_putstr(selected ? "30;42m" : "32m"));
 	if ((options & OPT_YELLOW) == OPT_YELLOW)
-		return (ft_putstr("33m"));
+		return (ft_putstr(selected ? "30;43m" : "33m"));
 	if ((options & OPT_BLUE) == OPT_BLUE)
-		return (ft_putstr("34m"));
+		return (ft_putstr(selected ? "30;44m" : "34m"));
 	if ((options & OPT_MAGENTA) == OPT_MAGENTA)
-		return (ft_putstr("35m"));
+		return (ft_putstr(selected ? "30;45m" : "35m"));
 	if ((options & OPT_CYAN) == OPT_CYAN)
-		return (ft_putstr("36m"));
+		return (ft_putstr(selected ? "30;46m" : "36m"));
 	ft_putstr("1m");
 }
 
 static void		print_weight(int options)
 {
-	if ((options & OPT_RE_SCREEN) == OPT_RE_SCREEN)
-		return (ft_putstr("[31;"));
 	if ((options & OPT_BOLD) == OPT_BOLD)
 		return (ft_putstr("[1;"));
 	if ((options & OPT_ITALIC) == OPT_ITALIC)
 		return (ft_putstr("[3;"));
 	if ((options & OPT_UNDERLINE) == OPT_UNDERLINE)
 		return (ft_putstr("[4;"));
-	ft_putstr("[");
+	ft_putstr("[0;");
 }
 
-static char		*get_extension(char *filename)
-{
-	char	*extension;
-
-	extension = NULL;
-	while ((filename = ft_strchr(filename,'.')))
-		extension = ++filename;
-	return (extension);
-}
-	
 int				get_colors(char *filename, int index)
 {
 	char	*extension;
@@ -64,14 +53,19 @@ int				get_colors(char *filename, int index)
 		flags |= OPT_UNDERLINE;
 	if (is_selected(index))
 		flags |= OPT_RE_SCREEN;
-	if (!(extension == get_extension(filename)))
-		return (flags);
+	extension = NULL;
+	while ((filename = ft_strchr(filename,'.')))
+		extension = ++filename;
+	if (!extension)
+		return (flags | OPT_YELLOW);
 	if (*extension == 'c' && !*(extension + 1))
-		flags |= OPT_YELLOW;
-	if (*extension == 'o' && !*(extension + 1))
+		flags |= OPT_RED;
+	else if (*extension == 'o' && !*(extension + 1))
 		flags |= OPT_MAGENTA;
-	if (*extension == 'h' && !*(extension + 1))
+	else if (*extension == 'h' && !*(extension + 1))
 		flags |= OPT_CYAN;
+	else
+		flags |= OPT_GREEN;
 	return (flags);
 }
 
